18971f6816268679d25e7e3adf770e4d20ea6fcf
simon <simon.bergsten@hotmail.com>
2020-12-15 17:59:53 +0100

    added stacks

---
+++ Stacks.js
@@ -0,0 +1,48 @@
+/*
+* Big 0:
+* Insertion - O(1)
+* Removal - O(1)
+* Searching - O(N)
+* Access - O(N)
+* */
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val){
+        var newNode = new Node(val);
+        if (!this.first){
+            this.first = val;
+            this.last = val;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+
+    pop(){
+        if (!this.first) return null;
+        var temp = this.first;
+
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+
+}
